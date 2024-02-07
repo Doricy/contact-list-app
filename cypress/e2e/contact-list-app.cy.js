7f07b05ea0c323330ed33274eb81aa9c86d8f565
Doris Addai <doricyj@gmail.com>
2024-02-07 08:24:39 +0000

    filling the text fields

--- cypress/e2e/contact-list-app.cy.js
+++ cypress/e2e/contact-list-app.cy.js
@@ -1,5 +1,12 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('should fill the form, submit, and assert that everything works ', () => {
+
+    //visit the website
+    cy.visit('https://thinking-tester-contact-list.herokuapp.com/')
+
+    //filling the fields
+    cy.get('#email').type("doricyj@gmail.com")
+    cy.get('#password').type("doris123")
+
   })
 })
