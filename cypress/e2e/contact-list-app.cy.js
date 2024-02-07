e0a7d4ef92c34a1b3ee00c1d06d1854a0bfc8b2a
Doris Addai <doricyj@gmail.com>
2024-02-07 08:48:34 +0000

    button submission and form assertion

--- cypress/e2e/contact-list-app.cy.js
+++ cypress/e2e/contact-list-app.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('should fill the form, submit, and assert that everything works ', () => {
+describe('login testing', () => {
+  it('should allow signed-in users to login and display success message', () => {
 
     //visit the website
     cy.visit('https://thinking-tester-contact-list.herokuapp.com/')
@@ -7,6 +7,15 @@ describe('template spec', () => {
     //filling the fields
     cy.get('#email').type("doricyj@gmail.com")
     cy.get('#password').type("doris123")
+
+    // Click the submit button
+    cy.get('button[type="submit"]').click();
+
+    // Assert that the form submission is successful
+    cy.url().should('include', '/success');
+    cy.contains('Login successful').should('exist');
+
+    
 
   })
 })
